7b7b4227156aa302364140bd937b750bc5c62be3
hidenorly <twitte.harold@gmail.com>
2016-04-11 00:22:48 +0900

    Initial commit of md2html.py, LICENSE and README.md

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# md2html
-A converter from Markdown to HTML with email (Content-ID) support
+# What's md2html.py?
+
+It's a converter from markdown to html.
+Remarkable points are considered to use the converted result as html mail.
+In short, this tool can convert link as Content-ID for html email.
+
+# How to use
+
+## Preparation
+
+```
+$ pip install markdown
+$ pip install py-gfm
+```
+
+Prepare preferred css file such as http://qiita.com/gamura@github/items/cda556061b6af247b9bc, https://github.com/sindresorhus/github-markdown-css, etc.
+
+## Basic usage
+
+```
+$ md2html.py hoge.md -o hoge.html -s bootstrap-md.css -t "Hoge"
+```
+
+```
+$ md2html.py hoge.md -o hoge.html -s bootstrap-md.css -t "Hoge" -m email
+```
+
+
+```
+Usage: md2html.py [options]
+
+Options:
+  -h, --help            show this help message and exit
+  -t TITLE, --title=TITLE
+                        Specify title
+  -c CHARSET, --charset=CHARSET
+                        Specify charset
+  -o OUTFILENAME, --output=OUTFILENAME
+                        Specify output filename
+  -s CSS, --css=CSS     Specify css filename
+  -m MODE, --mode=MODE  Specify web or email
+  -r REPLACERS, --replace=REPLACERS
+                        Specify replace key=value,...
+  -b, --bodyonly        --b if no header/footer
+```
+
+# Advanced usage
+
+With[mail.py](https://github.com/hidenorly/mailpy), 
+
+```
+$ md2html.py hoge.md -o hoge.html -s bootstrap-md.css -t "Hoge" -m email
+$ cat hoge.html | mail.py -r bootstrap-md.css -s "hoge" -t html hoge@gmail.com
+```
---
+++ md2html.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+# coding: utf-8
+#
+# Copyright (C) 2016 hidenorly
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import markdown
+from optparse import OptionParser, OptionValueError
+import sys
+import codecs
+
+cset = 'utf-8'
+
+reload(sys)
+sys.setdefaultencoding(cset)
+sys.stdin = codecs.getreader(cset)(sys.stdin)
+sys.stdout = codecs.getwriter(cset)(sys.stdout)
+
+html_header = '''<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="'''
+html_header_cset_title = '''">
+	<title>'''
+html_header_title_css = '''</title>
+	<link rel="stylesheet" type="text/css" href="'''
+html_header_css_body = '''">
+</head>
+<body>
+'''
+
+html_fotter = '''
+</body>
+</html>
+'''
+
+def fileRead(filename):
+	if filename == sys.stdin:
+		reader = codecs.getreader(cset)(filename)
+	else:
+		reader = open(filename,"r")
+
+	buf = ""
+	for line in reader:
+		buf = buf + line
+
+	return buf
+
+def fileWriter(filename, buf):
+	if filename == sys.stdout:
+		writer = codecs.getwriter(cset)(filename)
+	else:
+		writer = open(filename,"w")
+	writer.write(buf)
+	writer.close()
+
+def replaceResult(inBuf, replacers):
+	result = inBuf
+
+	for aReplacer in replacers:
+		posE = aReplacer.find("=")
+		if posE!=-1:
+			key = aReplacer[0:posE]
+			keyLen = len(key)
+			value = aReplacer[posE+1:len(aReplacer)]
+			pos = 0
+			while pos!=-1:
+				pos = result.find(key, pos)
+				if pos!=-1:
+					result = result[0:pos] + value + result[pos+keyLen:len(result)]
+					pos = pos + keyLen
+
+	return result
+
+def addPreStringIfNeed(result, key, preVal, pos):
+	pos = result[0].find(key, pos)
+	if pos!=-1:
+		pos = pos + len(key) + 1
+		if not result[0][pos:pos+7] == "http://" and not result[0][pos:pos+8] == "https://":
+			# found relative link, should add cid:
+			result[0] = result[0][0:pos] + preVal + result[0][pos:len(result[0])]
+	return pos
+
+
+def linkConvert(inBuf):
+	result = [inBuf]
+
+	pos = 0
+	pos2 = 0
+	while pos!=-1 or pos2!=-1:
+		# href link
+		pos = addPreStringIfNeed(result, "href=", "cid:", pos)
+		pos2 = addPreStringIfNeed(result, "src=", "cid:", pos2)
+
+	return result[0]
+
+
+if __name__ == '__main__':
+	md = markdown.Markdown(extensions=['gfm'])
+	parser = OptionParser()
+
+	parser.add_option("-t", "--title", action="store", type="string", dest="title", default="", help="Specify title")
+	parser.add_option("-c", "--charset", action="store", type="string", dest="charset", default=cset, help="Specify charset")
+	parser.add_option("-o", "--output", action="store", type="string", dest="outFilename", default=sys.stdout, help="Specify output filename")
+	parser.add_option("-s", "--css", action="store", type="string", dest="css", default="bootstrap-md.css", help="Specify css filename")
+	parser.add_option("-m", "--mode", action="store", type="string", dest="mode", default="web", help="Specify web or email")
+	parser.add_option("-r", "--replace", action="store", type="string", dest="replacers", default="", help="Specify replace key=value,...")
+	parser.add_option("-b", "--bodyonly", action="store_true", dest="bodyonly", default=False, help="--b if no header/footer")
+
+	(options, args) = parser.parse_args()
+
+	inFilename = sys.stdin
+	if args:
+		inFilename = args[0]
+
+	# set charset
+	cset = options.charset
+
+	# get key-value for replacement
+	replacers = []
+	if options.replacers.find(",")!=-1:
+		replacers = options.replacers.split(",")
+	else:
+		replacers = [ options.replacers ]
+
+	# read markdown
+	buf = fileRead(inFilename)
+
+	# convert markdown
+	result = md.convert(buf)
+
+	# create complete html
+	if not options.bodyonly:
+		result = html_header + options.charset + html_header_cset_title + options.title + html_header_title_css + options.css + html_header_css_body + result + html_fotter
+
+	# replace href link
+	if options.mode.find("email")!=-1:
+		result = linkConvert(result)
+
+	# replace with specified replacer
+	result = replaceResult(result, replacers)
+
+	# output the processed html
+	fileWriter( options.outFilename, result )
